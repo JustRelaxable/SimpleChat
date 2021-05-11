4a1802d146dea5c9bfa9ca22a9c809d26f057832
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2021-05-11 11:40:11 +0300

    Initial commit

---
+++ SimpleChatServerSideCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Linq;
+
+namespace ServerSideCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Use WebAPI class in order to reach API methods.
+            Console.WriteLine(WebAPI.instance.WeatherForecastAsync().Result);
+        }
+    }
+}
---
+++ SimpleChatServerSideCode/WebAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSideCode
+{
+    static class WebAPI
+    {
+        public static WebServicesAPI instance { get; private set; }
+        static WebAPI()
+        {
+            instance = new WebServicesAPI("https://localhost:44387/", new HttpClient());
+        }
+    }
+}
